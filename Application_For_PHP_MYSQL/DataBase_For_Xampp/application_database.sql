2d0a547fe980b481755387f00f76ed982c9db180
Mohamed Nour Abdo <abdonoor684@gmail.com>
2025-07-26 23:32:31 +0300

    Upload ToDoList File PHP

--- Application_For_PHP_MYSQL/DataBase_For_Xampp/application_database.sql
+++ Application_For_PHP_MYSQL/DataBase_For_Xampp/application_database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 10, 2025 at 10:38 PM
+-- Generation Time: Jul 26, 2025 at 09:29 PM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 7.4.16
 
@@ -24,6 +24,18 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `todolist`
+--
+
+CREATE TABLE `todolist` (
+  `id` int(11) NOT NULL,
+  `text` varchar(255) DEFAULT NULL,
+  `id_user` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user`
 --
 
@@ -39,8 +51,22 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `email`, `password`, `gender`, `activited`, `security`, `role`) VALUES
+(1, 'mmmm', 'abdonoor684@gmail.com', '88ea39439e74fa27c09a4fc0bc8ebe6d00978392', 0, 1, 'b67f692eff51788b7aa758a7e46b07ff', 'USER');
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `todolist`
+--
+ALTER TABLE `todolist`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_this_user` (`id_user`);
 
 --
 -- Indexes for table `user`
@@ -53,10 +79,26 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `todolist`
+--
+ALTER TABLE `todolist`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `todolist`
+--
+ALTER TABLE `todolist`
+  ADD CONSTRAINT `fk_this_user` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
